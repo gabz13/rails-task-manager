bc0c5698f65b26bc5b8a5d700a553d7fbc6c1800
gabrielle simon <gabriellesimon23@gmail.com>
2019-06-17 17:55:45 +0300

    finished task manager

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,35 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get 'tasks', to: 'tasks#index'
+  # post 'tasks', to: 'tasks#create'
+  # get 'tasks/new', to: 'tasks#new', as: :new_task
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # patch 'tasks/:id', to: 'tasks#update'
+  # delete 'tasks/:id', to: 'tasks#destroy'
 
-  # Read all tasks
-  get 'tasks', to: 'tasks#index'
-  # read one task
-  get 'tasks/:id', to: 'tasks#show', as: :task
-
-  # create one task (2 requests)
-  # 1st request to get the form
-  get 'tasks/new', to: 'tasks#new', as: :new_task
-  # 2nd request to post the params
-  post 'tasks', to: 'tasks#create'
-
-  # update on task
-  # get edit form
-  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
-  # post params and update db
-  patch 'tasks/:id', to: 'tasks#update'
-
-  # delete on tasks
-  delete 'tasks/:id', to: 'tasks#destroy'
+  resources :tasks
 end
+
+# Rails.application.routes.draw do
+#   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+#   # Read all tasks
+#   get 'tasks', to: 'tasks#index'
+#   # read one task
+#   get 'tasks/:id', to: 'tasks#show', as: :task
+
+#   # create one task (2 requests)
+#   # 1st request to get the form
+#   get 'tasks/new', to: 'tasks#new', as: :new_task
+#   # 2nd request to post the params
+#   post 'tasks', to: 'tasks#create'
+
+#   # update on task
+#   # get edit form
+#   get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+#   # post params and update db
+#   patch 'tasks/:id', to: 'tasks#update'
+
+#   # delete on tasks
+#   delete 'tasks/:id', to: 'tasks#destroy'
+# end
--- db/migrate/20190617105001_create_tasks.rb
+++ db/migrate/20190617105001_create_tasks.rb
@@ -2,7 +2,7 @@ class CreateTasks < ActiveRecord::Migration[5.2]
   def change
     create_table :tasks do |t|
       t.string :title
-      t.text :details
+      t.string :details
       t.boolean :completed, default: false
 
       t.timestamps
